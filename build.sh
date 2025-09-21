9e26f0c49978aa3b0cd7e7138d3939d6567f6cba
zonaapk <sintv18@gmail.com>
2025-09-20 21:06:58 -0300

    Add files via upload

--- build.sh
+++ build.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-# Salir si un comando falla
-set -o errexit
+set -e
 
-# Instalar las dependencias
 pip install -r requirements.txt
-
-# Crear la base de datos (si no existe)
 python -c "from app import db; db.create_all()"
